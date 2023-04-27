24d1f5a7fbd1e48b99137b931b2bd6d30990e0cb
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-04-26 19:14:24 -0600

    Mezcla de cartas

--- Lab8/Modular/src/components/Tablero.js
+++ Lab8/Modular/src/components/Tablero.js
@@ -1,19 +1,43 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import shuffle from 'lodash.shuffle';
 import Cartas from "./Cartas.js";
 import { images } from '../imagenes.js';
 import "./styles/Tablero.css";
 
 export default class Tablero extends Component {
-  render() {
+  state = {
+    cards: []
+  }
+
+  shuffleArray = (array) => {
+    let newArray = array.slice(0, 16);
+    for (let i = newArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = newArray[i];
+      newArray[i] = newArray[j];
+      newArray[j] = temp;
+    }
+    return newArray;
+  }
+
+  componentDidMount() {
     const cartas = images.map((image) => ({
       lenguaje: image.lenguaje,
       id: image.id,
       src: image.src,
     }));
 
+    const cartasDuplicadas = cartas.concat(cartas);
+
+    const cartasMezcladas = this.shuffleArray(cartasDuplicadas);
+
+    this.setState({ cards: cartasMezcladas });
+  }
+
+  render() {
     return (
       <div id="tablero">
-        {cartas.map((carta) => (
+        {this.state.cards.map((carta) => (
           <Cartas name={carta.lenguaje} number={carta.id} frontFace={carta.src} />
         ))}
       </div>
